9a1704e7b5ff1d86a95da0297317178629179bbf
dkothari <dkothari@itron.com>
2019-03-02 16:25:17 +0530

    second change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule, MatTabsModule, MatSidenavModule, MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
+import { RoutingModule } from './routing/routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavListComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './../components/home/home.component';
+import { AboutusComponent } from './../components/aboutus/aboutus.component';
+import { ContactusComponent } from './../components/contactus/contactus.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'contactus', component: ContactusComponent }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+export class RoutingModule { }
